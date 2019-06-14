6091d6d6227056e6cf6cb199a3c8cd5ba512c5ee
marwanaboudaoud <marwanaboudoud@gmail.com>
2019-06-14 09:07:06 +0200

    quotes machine project

---
+++ quotes/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import QuoteMachine from './quotesMachine'
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <QuoteMachine />
+    </div>
+  )
+}
+
+export default App
